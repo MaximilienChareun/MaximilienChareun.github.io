ed94ff8258efb8eaac83a06b569e15d06e386363
Max | Sika <97222624+MaximilienChareun@users.noreply.github.com>
2024-03-14 16:09:44 +0100

    Add files via upload

--- AntiChoco.py
+++ AntiChoco.py
@@ -1,10 +1,11 @@
+import tkinter as tk
 import keyboard
-import os
+import subprocess
 import time
 
 # Fonction pour mettre l'ordinateur en veille
 def mettre_en_veille():
-    os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+    subprocess.Popen(["rundll32.exe", "user32.dll,LockWorkStation"])
 
 # Drapeau pour indiquer s'il y a eu une activité du clavier
 activite_clavier = False
@@ -19,19 +20,51 @@ def detecter_activite_clavier(event):
 # Enregistrer la fonction de rappel pour l'activité clavier
 keyboard.on_press(detecter_activite_clavier)
 
-try:
-    while True:
-        # Vérifier s'il y a eu une activité du clavier
-        if activite_clavier:
-            activite_clavier = False  # Réinitialiser le drapeau
-            derniere_activite = time.time()
+# Fonction pour quitter le programme
+def quitter():
+    root.destroy()
 
-        # Vérifier si le délai de 2 minutes est écoulé sans activité
-        if time.time() - derniere_activite > 120:
-            print("ANTI-CHOCOBLAST ! Pas de pain au raisin pour Manu !!!!")
-            mettre_en_veille()  # Mettre en veille si aucun activité détectée
-            derniere_activite = time.time()
+# Fonction principale
+def main():
+    global activite_clavier, derniere_activite
+    # Vérifier s'il y a eu une activité du clavier
+    if activite_clavier:
+        activite_clavier = False  # Réinitialiser le drapeau
+        print("Clavier")
+        derniere_activite = time.time()
 
-        time.sleep(1)  # Garder le script en cours d'exécution
-except KeyboardInterrupt: 
-    keyboard.unhook_all()  # Désenregistrer tous les hooks de clavier
+    # Vérifier si le délai de 1 minute est écoulé sans activité
+    if time.time() - derniere_activite > 60:
+        print("ANTI-CHOCOBLAST ! Pas de pain au raisin pour Manu !!!!")
+        mettre_en_veille()  # Mettre en veille si aucun activité détectée
+        derniere_activite = time.time()
+        root.destroy()
+
+    root.after(1000, main)  # Appeler la fonction main() toutes les 1000 millisecondes
+
+# Configuration de la fenêtre principale
+root = tk.Tk()
+
+# Obtenir la résolution de l'écran
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Calculer les coordonnées pour placer la fenêtre en haut à droite de l'écran
+x = screen_width - 300
+y = 0
+
+# Définir la géométrie de la fenêtre
+root.geometry("150x80+{}+{}".format(x, y))
+
+# Style personnalisé pour la fenêtre de dialogue
+root.configure(bg="#f0f0f0")  # Couleur de fond
+root.attributes("-topmost", True)  # Toujours afficher la fenêtre au-dessus des autres
+root.overrideredirect(True)  # Supprimer la barre de titre et les bordures de la fenêtre
+
+# Contenu de la fenêtre
+quitter_button = tk.Button(root, text="Quitter", command=quitter, bg="#E4080A")
+quitter_button.pack(pady=5)
+
+# Lancer la boucle principale
+root.after(1000, main)  # Démarrer la fonction main() toutes les 1 ms
+root.mainloop()
